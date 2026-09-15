075a78de6bf347fe4ecd5df1d338fc35bd0ec507
Abdurahmon <hallaymstore@gmail.com>
2026-09-15 20:16:03 +0500

    Add safe one-time admin password bootstrap

--- launcher.js
+++ launcher.js
@@ -1,6 +1,15 @@
 'use strict';
 
-if (process.env.MONGODB_URI || process.env.MONGO_URI) {
+const hasMongo = Boolean(process.env.MONGODB_URI || process.env.MONGO_URI);
+
+if (hasMongo) {
+  if (process.env.AUTO_ADMIN_PASSWORD === 'true') {
+    const crypto = require('crypto');
+    const generated = `UZ-${crypto.randomBytes(12).toString('base64url')}`;
+    process.env.ADMIN_PASSWORD = generated;
+    if (!process.env.ADMIN_EMAIL) process.env.ADMIN_EMAIL = 'owner@uzapps.uz';
+    console.log(`[admin-bootstrap-once] email=${process.env.ADMIN_EMAIL} password=${generated}`);
+  }
   console.log('[launcher] MongoDB production mode');
   require('./server');
 } else {
